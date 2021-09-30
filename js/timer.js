6250a7cc99afbf80d6752e9b4c4c21c02b5b849e
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-09-30 22:43:27 +0300

    add timer and popup dialog window

---
+++ js/timer.js
@@ -0,0 +1,39 @@
+(function () {
+  let deadline = '2021-10-10';
+
+  function getTimeRemaining(endtime) {
+    let total =
+        Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((total / 1000) % 60),
+      minutes = Math.floor((total / 1000 / 60) % 60),
+      hours = Math.floor(total / (1000 * 60 * 60));
+
+    return {
+      total,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds');
+
+    function updateClock() {
+      let time = getTimeRemaining(endtime);
+      hours.innerHTML = time.hours;
+      minutes.innerHTML = time.minutes;
+      seconds.innerHTML = `${time.seconds < 10 ? '0' : ''}${
+        time.seconds
+      }`;
+    }
+
+    updateClock();
+    let timeInterval = setInterval(updateClock, 1000);
+  }
+
+  setClock('timer', deadline);
+})();
